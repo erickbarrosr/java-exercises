dd5d03db2be54257254104ba890a582c91e009ee
Erick Rezende <erickbarrosrezende@gmail.com>
2025-05-24 18:01:46 -0300

    feat: Resolver o exercício 3 da lista sequencial

---
+++ Exercise3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int firstValue = sc.nextInt();
+        int secondValue = sc.nextInt();
+        int thirdValue = sc.nextInt();
+        int fourthValue = sc.nextInt();
+
+        int difference = (firstValue * secondValue) - (thirdValue * fourthValue);
+
+        System.out.println("DIFERENÇA = " + difference);
+
+        sc.close();
+    }
+}
