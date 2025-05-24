8ee4019bebe33177b0100b37d9ccab0f294e7e7f
Erick Rezende <erickbarrosrezende@gmail.com>
2025-05-24 17:41:42 -0300

    feat: CResolver o exercício 1 da lista sequencial

---
+++ Exercise1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int firstValue = sc.nextInt();
+        int secondValue = sc.nextInt();
+
+        int response = firstValue + secondValue;
+
+        System.out.println("SOMA = " + response);
+
+        sc.close();
+    }
+}
