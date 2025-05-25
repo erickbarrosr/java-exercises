d7b24545d2b563a678d2be3e051d3bb43b2ae855
Erick Rezende <erickbarrosrezende@gmail.com>
2025-05-25 13:04:43 -0300

    feat: Resolver o exercício 4 da lista sequencial

---
+++ Exercise4.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int employeeNumber = sc.nextInt();
+        int workedHours = sc.nextInt();
+        double valuePerHour = sc.nextDouble();
+        double salary = workedHours * valuePerHour;
+
+        System.out.println("NUMBER = " + employeeNumber);
+        System.out.printf("SALARY = U$ %.2f%n", salary);
+
+        sc.close();
+    }
+}
