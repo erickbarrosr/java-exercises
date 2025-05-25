2e3d3bfb37c47fd943421dd67d332b87b75bbde9
Erick Rezende <erickbarrosrezende@gmail.com>
2025-05-25 13:43:16 -0300

    feat: Resolver o exercício 5 da lista sequencial

---
+++ Exercise5.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int partCodeOne = sc.nextInt();
+        int partQtyOne = sc.nextInt();
+        double partUnitValueOne = sc.nextDouble();
+        double partTotalValueOne = partQtyOne * partUnitValueOne;
+
+        int partCodeSecond = sc.nextInt();
+        int partQtySecond = sc.nextInt();
+        double partUnitValueSecond = sc.nextDouble();
+        double partTotalValueSecond = partQtySecond * partUnitValueSecond;
+
+        double valueToPay = partTotalValueOne + partTotalValueSecond;
+
+        System.out.printf("VALOR À PAGAR: R$ %.2f%n", valueToPay);
+
+        sc.close();
+    }
+}
